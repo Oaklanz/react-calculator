7e38b1cb2cb2193257970c5553103617d146dd1d
Oaklanz <69703980+Oaklanz@users.noreply.github.com>
2021-06-05 12:29:30 -0400

    wrote the codebase for the first input box

--- react-calculator/src/components/Calculator.js
+++ react-calculator/src/components/Calculator.js
@@ -1,3 +1,23 @@
 import React, {useState} from 'react'
 
-export default Calculator
+
+const Calculator = () => {
+
+  const [number, setNumber] = useState('')
+
+  const upDateNumber = (event) => {
+      setNumber(event.target.value)
+  }
+
+    return (
+        <div className="maths">
+
+           <h2>React Calculator</h2> 
+           <input type="text" number="search" onChange={(event) => setNumber(event.target.value)} value={number}/>
+        </div>
+    )
+    
+}
+
+
+export default Calculator;
