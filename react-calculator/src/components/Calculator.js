3eb74a35790316803bf6813be14c3730e4a88bae
Oaklanz <69703980+Oaklanz@users.noreply.github.com>
2021-06-06 11:18:57 -0400

    wrote the codebase for the first input box

--- react-calculator/src/components/Calculator.js
+++ react-calculator/src/components/Calculator.js
@@ -3,18 +3,60 @@ import React, {useState} from 'react'
 
 const Calculator = () => {
 
-  const [number, setNumber] = useState('')
+  const [operand1, setOperand1] = useState('')
+  const [operand2, setOperand2] = useState('')
+  const [operation, setOperation] = useState('')
+  const [answer, setAnswer] = useState('')
+    
 
   const upDateNumber = (event) => {
-      setNumber(event.target.value)
+      setOperand1(event.target.value)
   }
 
     return (
-        <div className="maths">
+        <div>
+            
+   
+          <div>
+              <div className="maths">
+                  <h2>React Calculator</h2> 
+                   <input type="text" id="operand1" value={operand1} onChange= {event => setOperand1(event.target.value)}/>
+                   <span> </span>
+                   <select id="operator">
+                       <option value="add">+</option>
+                       <option value="subtract">-</option>
+                       <option value="multiply">*</option>
+                       <option value="divide">/</option>
+                   </select>
+                   <span> </span>
+                   <input type="text" id="num2" />
+                   <span> </span>
+                   <input type="button" value="=" onClick="calculate()" />
+                   <span> </span>
+                   <input type="text" id="answer" />
+                 
+                 
+                               
+                  function calculate() {
 
-           <h2>React Calculator</h2> 
-           <input type="text" number="search" onChange={(event) => setNumber(event.target.value)} value={number}/>
-        </div>
+                  <div>
+                        <var num1 = "getElementById(num1).value" />
+                             
+                      <var num2 = "getElementById(num2).value" />
+                                   
+                      <var num3 = "parseInt(num1) + parseInt(num2)"/> 
+                  </div>
+                   
+                  } 
+                 
+                </div>
+                 
+            </div>
+            
+
+        </div>        
+        
+
     )
     
 }
